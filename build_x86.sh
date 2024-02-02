1b44a37f4e82b620a838290d32d857a8f8b58177
Jian Li <pyguni@gmail.com>
2024-02-01 20:07:34 -0500

    Replace all helm charts with bitnami's charts, support multi builds

---
+++ build_x86.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -ex
+set -o pipefail
+
+TAG=${TAG:-v3.3.2-amd64}
+REPO=${REPO:-registry.gitlab.com/sonaproject}
+
+# support other container tools. e.g. podman
+CONTAINER_CLI=${CONTAINER_CLI:-docker}
+CONTAINER_BUILDER=${CONTAINER_BUILDER:-"buildx build"}
+
+# supported platforms
+PLATFORMS=linux/amd64
+
+# shellcheck disable=SC2086 # inteneded splitting of CONTAINER_BUILDER
+${CONTAINER_CLI} ${CONTAINER_BUILDER} \
+  --platform ${PLATFORMS} \
+  --push \
+  -f Dockerfile \
+  -t "${REPO}"/openpitrix-jobs:"${TAG}" .
